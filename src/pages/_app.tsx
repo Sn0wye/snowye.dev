b68fd8522604c8990ece67974b700be0cfe75e34
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-07-21 19:04:58 -0300

    feat: :sparkles: wrap app in layout and providers

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,15 @@
 import type { AppProps } from 'next/app';
+import CommandBar from '../components/CommandBar';
+import { Layout } from '../components/Layout';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <CommandBar>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </CommandBar>
+  );
 }
 
 export default MyApp;
