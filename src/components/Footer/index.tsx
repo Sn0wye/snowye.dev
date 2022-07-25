93d018f85aa3062d6e00124d4e8d11a582a55142
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-07-25 11:18:13 -0300

    feat: :sparkles: add footer and styling

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,67 @@
+import Link from 'next/link';
+import { BsGithub, BsLinkedin } from 'react-icons/bs';
+import { FaInstagram } from 'react-icons/fa';
+import { RiMailLine } from 'react-icons/ri';
+import { Anchor, Container, Title } from './styles';
+
+interface Link {
+  title: string;
+  url: string;
+  icon: JSX.Element;
+  variant: Variants;
+}
+
+type Variants = 'instagram' | 'email' | 'github' | 'linkedin';
+
+const links: Link[] = [
+  {
+    title: 'Email',
+    url: '/contact',
+    icon: <RiMailLine />,
+    variant: 'email',
+  },
+  {
+    title: 'GitHub',
+    url: 'https://github.com/Sn0wye',
+    icon: <BsGithub />,
+    variant: 'github',
+  },
+  {
+    title: 'linkedin',
+    url: 'https://linkedin.com/in/gabriel-trzimajewski',
+    icon: <BsLinkedin />,
+    variant: 'linkedin',
+  },
+  {
+    title: 'Instagram',
+    url: 'https://instagram.com/gabtrzimajewski',
+    icon: <FaInstagram />,
+    variant: 'instagram',
+  },
+];
+
+export function Footer() {
+  const renderLink = (link: Link, index: number) => {
+    if (link.url.startsWith('http')) {
+      return (
+        <Anchor key={index} href={link.url} target='_blank' type={link.variant}>
+          <Title>{link.title}</Title>
+          {link.icon}
+        </Anchor>
+      );
+    }
+
+    return (
+      <Link key={index} href={link.url} passHref>
+        <Anchor type={link.variant}>
+          <Title>{link.title}</Title>
+          {link.icon}
+        </Anchor>
+      </Link>
+    );
+  };
+
+  return (
+    <Container>{links.map((link, index) => renderLink(link, index))}</Container>
+  );
+}
---
+++ src/components/Footer/styles.ts
@@ -0,0 +1,78 @@
+import { styled } from '../../../stitches.config';
+
+export const Container = styled('footer', {
+  background: '$background',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  padding: '1.25rem 0',
+});
+
+export const Anchor = styled('a', {
+  color: '$secondary',
+  display: 'flex',
+  alignItems: 'center',
+  gap: '0.3125rem',
+  fontSize: '1rem',
+  border: 0,
+  marginLeft: '1.25rem',
+  transition: 'color $duration ease-in-out',
+  '&:hover, &:focus': {
+    color: '$primary',
+    opacity: 1,
+  },
+  '& svg': {
+    marginBottom: '0.25rem',
+    fontWeight: 'bold',
+    fill: '$primary',
+    opacity: 0,
+    width: '1rem',
+    height: '1rem',
+    transition: 'opacity $durwation ease-in-out',
+    '@bp2': { opacity: 0, fontSize: '1rem' },
+  },
+  '&:hover svg': {
+    opacity: 1,
+  },
+  variants: {
+    type: {
+      linkedin: {
+        '&:hover': {
+          color: '$linkedin',
+        },
+        '& svg': {
+          fill: '$linkedin',
+        },
+      },
+      email: {
+        '&:hover': {
+          color: '$primary',
+        },
+        '& svg': {
+          fill: '$primary',
+        },
+      },
+      github: {
+        '&:hover': {
+          color: '$github',
+        },
+        '& svg': {
+          fill: '$github',
+        },
+      },
+      instagram: {
+        '&:hover': {
+          color: '$pink',
+        },
+        '& svg': {
+          fill: '$pink',
+        },
+      },
+    },
+  },
+});
+
+export const Title = styled('span', {
+  display: 'none',
+  '@bp2': { display: 'block' },
+});
