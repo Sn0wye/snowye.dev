8ad497a5548bc07bf094c7a146a7418c788657f7
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-07-28 14:42:15 -0300

    feat: :sparkles: add shortcut for opening cmd palette (home)

---
+++ src/components/OpenCommandPalette/index.tsx
@@ -0,0 +1,40 @@
+import { useKBar } from 'kbar';
+import { useEffect, useState } from 'react';
+import { Button } from '../styled/Button';
+
+export function OpenCommandPalette() {
+  const { query } = useKBar();
+  const [mounted, setMounted] = useState(false);
+
+  // This is a hack to make sure the command palette is mounted on render.
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (mounted) {
+    const isMac = /(Mac)/i.test(navigator.userAgent);
+    const isMobile = /iPhone|iPad|Android/i.test(navigator.userAgent);
+
+    if (isMobile) {
+      return (
+        <Button type='button' onClick={query.toggle}>
+          Tap to start →
+        </Button>
+      );
+    }
+    if (isMac) {
+      return (
+        <Button type='button' onClick={query.toggle}>
+          Press <kbd>⌘</kbd> <kbd>K</kbd> to start →
+        </Button>
+      );
+    }
+
+    //Common cases (Windows, Linux)
+    return (
+      <Button type='button' onClick={query.toggle}>
+        Press <kbd>ctrl</kbd> <kbd>K</kbd> to start →
+      </Button>
+    );
+  }
+}
