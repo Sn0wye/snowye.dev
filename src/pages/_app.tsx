6a05398f0f20f7a25b1db54ca7f45710af8b1806
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-08-05 21:24:28 -0300

    refactor: :fire: remove layout from app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,10 @@
 import type { AppProps } from 'next/app';
 import CommandBar from '../components/CommandBar';
-import { Layout } from '../components/Layout';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <CommandBar>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <Component {...pageProps} />
     </CommandBar>
   );
 }
