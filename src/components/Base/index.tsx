e44ae4bfda02312e41978fb0b25078fc5d425b58
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-08-05 21:23:46 -0300

    feat: :sparkles: add base component and its styling

---
+++ src/components/Base/index.tsx
@@ -0,0 +1,53 @@
+import { Footer } from '../Footer';
+import { Navbar } from '../Navbar';
+import { PostContainer, PostContent, PostMain } from '../styled/Post';
+import { GradientTitle, Wrapper } from './styles';
+
+interface BaseProps {
+  children: React.ReactNode;
+  title: string;
+  tagline: string;
+  primaryColor: string;
+  secondaryColor: string;
+}
+
+export function Base({
+  children,
+  primaryColor,
+  secondaryColor,
+  tagline,
+  title,
+}: BaseProps) {
+  return (
+    <Wrapper>
+      <Navbar />
+      <PostMain
+        css={{
+          '& ::selection': {
+            background: `$${primaryColor}`,
+            color: '#000',
+            WebkitTextFillColor: '#000',
+          },
+        }}
+      >
+        <PostContent>
+          <PostContainer>
+            <GradientTitle
+              css={{
+                backgroundImage: `linear-gradient(
+                135deg,
+                $${primaryColor} 0%,
+                $${secondaryColor} 100%
+              );`,
+              }}
+            >
+              {tagline ? tagline : title}
+            </GradientTitle>
+            {children}
+          </PostContainer>
+        </PostContent>
+      </PostMain>
+      <Footer />
+    </Wrapper>
+  );
+}
---
+++ src/components/Base/styles.ts
@@ -0,0 +1,19 @@
+import { styled } from '../../../stitches.config';
+
+export const GradientTitle = styled('h1', {
+  backgroundSize: '100%',
+  backgroundClip: 'text',
+  WebkitBackgroundClip: 'text',
+  MozBackgroundClip: 'text',
+  WebkitTextFillColor: 'transparent',
+  MozTextFillColor: 'transparent',
+  WebkitBoxDecorationBreak: 'clone',
+});
+
+export const Wrapper = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  minHeight: '100vh',
+  position: 'relative',
+  zIndex: 0,
+});
