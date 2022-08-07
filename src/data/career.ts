4735224dbf38864cdfb22bc1130d2708a52edab4
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-08-07 13:09:18 -0300

    feat: :sparkles: add career section component

---
+++ src/data/career.ts
@@ -0,0 +1,17 @@
+export const jobs = [
+  {
+    jobTitle: 'Junior Frontend Engineer Freelance',
+    company: 'Coff3e',
+    companyUrl: 'https://coff3e.dev/',
+    startDate: '2022-03-01',
+    endDate: '2022-04-01',
+    location: 'São Paulo, SP',
+  },
+  {
+    jobTitle: 'Your job title here',
+    company: 'Your company here',
+    companyUrl: 'https://your-company-here.com/',
+    startDate: new Date().toISOString().split('T')[0],
+    location: 'Your location here',
+  },
+];
---
+++ src/pages/about/Career.tsx
@@ -0,0 +1,44 @@
+import { format, parseISO } from 'date-fns';
+import { jobs } from '../../data/career';
+import { getDurationString } from '../../utils/getDurationString';
+
+export interface Job {
+  jobTitle: string;
+  company: string;
+  companyUrl: string;
+  startDate: string;
+  endDate?: string;
+  location: string;
+}
+
+export const Career = () => {
+  return (
+    <>
+      {jobs.map(
+        (
+          { company, companyUrl, jobTitle, location, startDate, endDate },
+          index
+        ) => (
+          <article style={{ marginBottom: 40 }} key={index}>
+            <h3>{jobTitle}</h3>
+            <p style={{ margin: 0 }}>
+              <a href={companyUrl} target='_blank' rel='noreferrer'>
+                {company}
+              </a>
+              <span> • {location}</span>
+            </p>
+            <p style={{ margin: 0 }}>
+              <span>{format(parseISO(startDate), 'LLL yyyy')}</span>
+              <span> – </span>
+              <span>
+                {endDate ? format(parseISO(endDate), 'LLL yyyy') : 'Present'}
+              </span>
+              <span> • </span>
+              <span>{getDurationString(startDate, endDate)}</span>
+            </p>
+          </article>
+        )
+      )}
+    </>
+  );
+};
