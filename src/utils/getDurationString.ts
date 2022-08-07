89d42511ad09f74848b3ca90c7a779226270bdf0
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-08-07 12:56:07 -0300

    feat: :sparkles: add getDurationString function

---
+++ src/utils/getDurationString.ts
@@ -0,0 +1,30 @@
+import { intervalToDuration, parseISO } from 'date-fns';
+
+export const getDurationString = (
+  startDate: string,
+  endDate: string | undefined
+) => {
+  const interval = {
+    start: parseISO(startDate),
+    end: endDate ? parseISO(endDate) : new Date(),
+  };
+  const durationObj = intervalToDuration(interval);
+
+  const { years, months } = durationObj;
+
+  let durationStr = '';
+
+  if (years !== undefined && months === undefined) {
+    throw new Error('Duration < 1 month');
+  }
+
+  if (years !== undefined && years >= 1) {
+    durationStr += `${years} yr${years > 1 ? 's' : ''} `;
+  }
+
+  if (months !== undefined && months >= 1) {
+    durationStr += `${months} mo${months > 1 ? 's' : ''}`;
+  }
+
+  return durationStr;
+};
