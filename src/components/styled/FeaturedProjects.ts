919d181424ffa7a9d34a425bc9ccc05328b8e68a
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-08-13 18:03:33 -0300

    style: add featured projects styling

---
+++ src/components/styled/FeaturedProjects.ts
@@ -0,0 +1,10 @@
+import { motion } from 'framer-motion';
+import { styled } from '../../../stitches.config';
+
+export const FeaturedProjects = styled(motion.div, {
+  margin: '0.625rem 0 0 -1.25rem',
+  display: 'flex',
+  flexWrap: 'wrap',
+  flexDirection: 'column',
+  '@bp2': { flexDirection: 'row' },
+});
