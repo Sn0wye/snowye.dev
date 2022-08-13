94098e008e919e86e54e55b2ddacaff217570c39
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-08-13 18:03:08 -0300

    feat:

---
+++ src/components/FeaturedProject/index.tsx
@@ -0,0 +1,49 @@
+import { Dispatch, SetStateAction } from 'react';
+import type { Project } from '../../data/projects';
+import {
+  Body,
+  Description,
+  Hover,
+  Project as StyledProject,
+  Stats,
+  Title,
+} from './styles';
+
+interface FeaturedProjectProps extends Project {
+  onHover: Dispatch<SetStateAction<string>>;
+  isHovered: boolean;
+}
+
+export function FeaturedProject({
+  title,
+  url,
+  description,
+  icon,
+  stats,
+  onHover,
+  isHovered,
+}: FeaturedProjectProps) {
+  return (
+    <StyledProject
+      href={url}
+      target='_blank'
+      onHoverStart={() => onHover(title)}
+      onHoverEnd={() => onHover('')}
+    >
+      {icon && icon}
+      <Body>
+        <Title>{title}</Title>
+        <Description>{description}</Description>
+        {stats && <Stats>{stats}</Stats>}
+      </Body>
+      {isHovered && (
+        <Hover
+          layoutId='projects'
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+        />
+      )}
+    </StyledProject>
+  );
+}
