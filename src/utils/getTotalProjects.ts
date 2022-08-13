f07956c3ca6b70b020fafdf98012f31bfe58d5fb
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-08-13 18:03:57 -0300

    feat: add getTotalProjects util

---
+++ src/utils/getTotalProjects.ts
@@ -0,0 +1,11 @@
+import { Projects } from '../data/projects';
+
+export const getTotalProjects = (projects: Projects) => {
+  let total = 0;
+
+  for (let i = 0; i < projects.length; i++) {
+    total = total + projects[i].projects.length;
+  }
+
+  return total;
+};
