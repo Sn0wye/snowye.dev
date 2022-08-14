3647745d9707aa3de4bf140f43d3b681e569f174
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-08-14 14:54:40 -0300

    chore: add initial projects data

---
+++ src/data/projects.tsx
@@ -0,0 +1,43 @@
+import { BsCardChecklist } from 'react-icons/bs';
+import { FaIdBadge } from 'react-icons/fa';
+import { RiMoneyDollarBoxFill } from 'react-icons/ri';
+
+export type Projects = {
+  year: string;
+  projects: Project[];
+}[];
+
+export interface Project {
+  title: string;
+  url: string;
+  description?: string;
+  icon?: JSX.Element;
+  stats?: string;
+}
+
+export const projects: Projects = [
+  {
+    year: '2022',
+    projects: [
+      {
+        title: 'Personal Portfolio',
+        url: 'https://github.com/Sn0wye/snowye.dev',
+        description: "This portfolio. It's open souce!",
+        icon: <FaIdBadge />,
+      },
+      {
+        title: 'iFinance',
+        url: 'https://', //TODO: Put the deploy link here
+        description: 'Finances app to keep track of your gains and dispenses',
+        icon: <RiMoneyDollarBoxFill />,
+      },
+      {
+        title: 'Spaces',
+        url: 'https://spaces-todo.netlify.app',
+        description:
+          'A simple and straightforward todo app to get your tasks done best.',
+        icon: <BsCardChecklist />,
+      },
+    ],
+  },
+];
