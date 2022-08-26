8a482f08388bdba9892485ba37883154bcb29976
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-08-26 11:09:42 -0300

    feat: add all projects test

---
+++ src/components/containers/Projects/AllProjects.spec.tsx
@@ -0,0 +1,22 @@
+import '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
+import { projects } from '../../../data/projects';
+import { AllProjects } from './AllProjects';
+
+describe('<AllProjects />', () => {
+  it('should render properly', () => {
+    const { container } = render(<AllProjects />);
+    expect(container).toBeInTheDocument();
+  });
+
+  it('should render items correctly', () => {
+    const { getByText } = render(<AllProjects />);
+
+    for (const project in projects) {
+      projects[project].projects.forEach((project) => {
+        expect(getByText(project.title)).toBeInTheDocument();
+      });
+    }
+  });
+});
