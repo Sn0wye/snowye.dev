ab0660df41e94a8e7e501804a8dd9bc4572d9540
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-11-27 14:27:15 -0300

    feat: add translations to about page

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,65 +1,58 @@
-import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import { Base } from '../../components/Base';
 import { Career } from '../../components/containers/About/Career';
 import { Intro } from '../../components/containers/About/Intro';
+import { getLocaleProps, useI18n } from '../../locales';
 import { stripHtml } from '../../utils/stripHtml';
 
 interface AboutProps {
-  title: string;
-  description: string;
-  tagline: string;
   imagePath: string;
   primaryColor: string;
   secondaryColor: string;
 }
 
 export default function About({
-  title,
-  description,
-  tagline,
   imagePath,
   primaryColor,
-  secondaryColor,
+  secondaryColor
 }: AboutProps) {
+  const { scopedT } = useI18n();
+  const t = scopedT('pages.about');
   return (
     <Base
-      tagline={tagline}
-      title={title}
+      tagline={t('tagline')}
+      title={t('title')}
       primaryColor={primaryColor}
       secondaryColor={secondaryColor}
     >
       <Head>
-        <title>{title}</title>
-        <meta content={title} property='og:title' />
-        <meta content={stripHtml(description)} name='description' />
-        <meta content={stripHtml(description)} property='og:description' />
+        <title>{t('title')}</title>
+        <meta content={t('title')} property='og:title' />
+        <meta content={stripHtml(t('description'))} name='description' />
+        <meta content={stripHtml(t('description'))} property='og:description' />
         <meta content='https://snowye.dev/about' property='og:url' />
         <meta content={`https://snowye.dev${imagePath}`} property='og:image' />
       </Head>
 
       <Intro />
 
-      <h2>Bio</h2>
+      <h2>{t('bio')}</h2>
       <blockquote>
-        <p>{description}</p>
+        <p>{t('description')}</p>
       </blockquote>
 
-      <h2>Career</h2>
+      <h2>{t('career')}</h2>
       <Career />
     </Base>
   );
 }
 
-export const getStaticProps: GetStaticProps = () => {
+export const getStaticProps = getLocaleProps(() => {
   const meta = {
-    title: 'About | Gabriel Trzimajewski',
-    description: `My name is Gabriel Trzimajewski. aka Snowye ✌️. I'm a Frontend Engineer on its own. I'm a guy who loves helping people and turning the world into a better place.`,
-    tagline: 'Code. Sleep. Repeat.',
     imagePath: '/static/imagePaths/snowye-bw.jpg',
     primaryColor: 'pink',
-    secondaryColor: 'purple',
+    secondaryColor: 'purple'
   };
 
   return { props: meta };
-};
+});
