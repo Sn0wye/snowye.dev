b6ea580b927c45a10d73ce5bcc755cbefd1d042a
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-11-27 14:28:37 -0300

    feat: add translations to about page

--- src/components/containers/About/Intro.tsx
+++ src/components/containers/About/Intro.tsx
@@ -1,8 +1,12 @@
 import Image from 'next/image';
+import { useI18n } from '../../../locales';
 import { Pronunciation } from '../../Pronunciation';
 import { Container, Paragraph, Section } from './styles';
 
 export const Intro = () => {
+  const { scopedT } = useI18n();
+  const t = scopedT('pages.about.intro');
+
   return (
     <Container>
       <Section>
@@ -20,35 +24,27 @@ export const Intro = () => {
         <Paragraph
           css={{
             marginTop: '1rem',
-            '@bp2': { marginTop: '-0.375rem' },
+            '@bp2': { marginTop: '-0.375rem' }
           }}
         >
-          <strong>Hey, I&apos;m Gabriel Trzimajewski!</strong>
-          <Pronunciation />I started simply browsing in the internet and
-          randomly discovered Javascript at{' '}
-          <strong>
-            <a
-              href='https://rocketseat.com.br'
-              target='_blank'
-              rel='noreferrer'
-            >
-              Rocketseat
-            </a>
-          </strong>{' '}
-          back on start of 2022.
+          <strong>{t('1')}</strong>
+          {t('2', {
+            pronunciation: <Pronunciation />,
+            link: (
+              <strong>
+                <a
+                  href='https://rocketseat.com.br'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  Rocketseat
+                </a>
+              </strong>
+            )
+          })}
         </Paragraph>
-        <Paragraph>
-          I&apos;m currently working on myself and trying to improve my hard and
-          soft skills aswell. I&apos;m from Brazil and a big fan of{' '}
-          <strong>twenty one pilots</strong>.
-        </Paragraph>
-        <Paragraph>
-          I believe the beauty of the world stand on the{' '}
-          <strong>little things</strong>. When I&apos;m not working, I like
-          going to the gym, hanging out with my friends on Discord, and of
-          course, coding side projects and helping the{' '}
-          <strong>open source</strong> community.
-        </Paragraph>
+        <Paragraph dangerouslySetInnerHTML={{ __html: t('3') }} />
+        <Paragraph dangerouslySetInnerHTML={{ __html: t('4') }} />
       </Section>
     </Container>
   );
