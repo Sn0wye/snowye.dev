767f44cf323ea825c8fceddaa93ac3a793096917
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-11-27 14:25:36 -0300

    feat: add i18n provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import type { AppProps } from 'next/app';
 import CommandBar from '../components/CommandBar';
+import { I18nProvider } from '../locales';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <CommandBar>
-      <Component {...pageProps} />
-    </CommandBar>
+    <I18nProvider locale={pageProps.locale}>
+      <CommandBar>
+        <Component {...pageProps} />
+      </CommandBar>
+    </I18nProvider>
   );
 }
 
