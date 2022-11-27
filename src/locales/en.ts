40c1a1378f4092c522bad00e64e7efd925f48f0a
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-11-27 14:25:06 -0300

    feat: add en locale

---
+++ src/locales/en.ts
@@ -0,0 +1,80 @@
+export default {
+  'common.navbar.about': 'About',
+  'common.navbar.projects': 'Projects',
+  'common.navbar.contact': 'Contact',
+
+  'common.kbar.start.mobile': 'Tap to start →',
+  'common.kbar.start.mac': 'Press {keys} to start →',
+  'common.kbar.start.pc': 'Press {keys} to start →',
+
+  'common.kbar.actions.copy': 'Copy URL',
+  'common.kbar.actions.email': 'Send Email',
+  'common.kbar.actions.source': 'View Source',
+  'common.kbar.actions.home': 'Home',
+  'common.kbar.actions.about': 'About',
+  'common.kbar.actions.projects': 'Projects',
+
+  'common.kbar.sections.general': 'General',
+  'common.kbar.sections.goto': 'Go To',
+  'common.kbar.sections.follow': 'Follow',
+
+  'common.kbar.toast.title': 'Copied :D',
+  'common.kbar.toast.description': 'You can now share it with anyone.',
+
+  'pages.home.title': 'Gabriel Trzimajewski',
+  'pages.home.meta': 'FrontEnd Engineer on its own.',
+  'pages.home.description':
+    'A guy who loves helping people and turning the world into a better place.',
+
+  'pages.about.title': 'About | Gabriel Trzimajewski',
+  'pages.about.bio': 'Bio',
+  'pages.about.description':
+    "My name is Gabriel Trzimajewski. AKA Snowye ✌️. I'm a Frontend Engineer on its own. I'm a guy who loves helping people and turning the world into a better place.",
+  'pages.about.tagline': 'Code. Sleep. Repeat.',
+  'pages.about.intro.1': "Hey, I'm Gabriel Trzimajewski!",
+  'pages.about.intro.2':
+    '{pronunciation}I started simply browsing in the internet and randomly discovered Javascript at {link} back on start of 2022.',
+  'pages.about.intro.3':
+    'I&apos;m currently working on myself and trying to improve my hard and soft skills aswell. I&apos;m from Brazil and a big fan of <strong>twenty one pilots</strong>.',
+  'pages.about.intro.4':
+    'I believe the beauty of the world stand on the <strong>little things</strong>. When I&apos;m not working, I like going to the gym, hanging out with my friends on Discord, and of course, coding side projects and helping the <strong>open source</strong> community, such as <strong><a href="https://create.t3.gg">Create T3 App</a></strong>, in which I was part of the documentation translation team into Brazilian Portuguese.',
+  'pages.about.career': 'Career',
+
+  'pages.projects.title': 'Projects | Gabriel Trzimajewski',
+  'pages.projects.description':
+    "Side projects are a way to convert your <strong>thoughts and personality</strong> into code. Here you can see the <strong>{totalProjects} different.</strong> apps, projects and libraries that I've been working on.",
+  'pages.projects.featured': 'Featured Projects',
+  'pages.projects.all': 'All Projects',
+
+  'pages.projects.portfolio.title': 'Personal Portfolio',
+  'pages.projects.portfolio.description': "This portfolio. It's open source!",
+
+  'pages.projects.ifinance.description':
+    'Finances app to keep track of your gains and expenses',
+
+  'pages.projects.spaces.description':
+    'A simple and straightforward todo app to get your tasks done best.',
+
+  'pages.projects.duquedev.description':
+    'Portfolio developed for Felipe Duque.',
+
+  'pages.contact.title': 'Contact | Gabriel Trzimajewski',
+  'pages.contact.tagline': 'Email me. As in the good old days.',
+  'pages.contact.description':
+    "<strong>I love chatting</strong> with software engineers, tech founders, students, and creators. <strong>I'm a busy person</strong>, so I can't promise that I'll reply to your email right away, but I'll try my best to respond in a timely manner.",
+  'pages.contact.email': 'Send an email',
+
+  'pages.contact.toast.success.title': 'Email sent :D',
+  'pages.contact.toast.success.description':
+    'Thanks for taking the time to write it.',
+
+  'pages.contact.toast.fail.title': 'Error :(',
+  'pages.contact.toast.fail.description':
+    'Something wrong happened. Try again later.',
+
+  'pages.contact.labels.name': 'name',
+  'pages.contact.labels.email': 'email',
+  'pages.contact.labels.message': 'message',
+  'pages.contact.placeholders.message': 'Your message here :P',
+  'pages.contact.send': 'Send'
+} as const;
