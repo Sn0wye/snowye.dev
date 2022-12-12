cbd526557de68e4fd95d623667fee976a6c0b1fa
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-12-12 17:48:55 -0300

    feat: add 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,44 @@
+import Lottie from 'lottie-react';
+import lottieAnimation from '../../public/static/icons/404.json';
+import { Layout } from '../components/Layout';
+
+const Box = styled('div', { boxSizing: 'border-box' });
+
+const NotFound = () => {
+  return (
+    <Layout>
+      <Box
+        css={{
+          padding: '$navHeightMobile 1.25rem',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          flex: 1
+        }}
+      >
+        <h1>four oh four</h1>
+        <Box
+          css={{
+            display: 'flex',
+            justifyContent: 'center'
+          }}
+        >
+          <Lottie
+            animationData={lottieAnimation}
+            loop={true}
+            autoPlay={true}
+            style={{ width: '60%' }}
+          />
+        </Box>
+        <p>This page doesn&apos;t exist</p>
+        <ShortcutError />
+      </Box>
+    </Layout>
+  );
+};
+
+export default NotFound;
+
+import { styled } from '../../stitches.config';
+import { ShortcutError } from '../components/ShortcutError';
