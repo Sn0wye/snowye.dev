747b8f6cefd9a40a055033b0a2bbbea91a3a2073
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-12-12 17:48:51 -0300

    feat: add error shortcut

---
+++ src/components/ShortcutError/index.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react';
+import { Button } from '../styled/Button';
+
+export const ShortcutError = () => {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (mounted) {
+    const isMobile = /iPhone|iPad|Android/i.test(navigator.userAgent);
+
+    if (isMobile) {
+      return (
+        <Button as='a' href='/'>
+          Tap to go home →
+        </Button>
+      );
+    }
+
+    return (
+      <Button as='a' href='/'>
+        Press <kbd>G</kbd> <kbd>H</kbd> to go home →
+      </Button>
+    );
+  }
+
+  return null;
+};
