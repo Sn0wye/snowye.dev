246fc9da306aa2d8fce8616a5bdf5fc9b7627bec
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-12-12 17:50:40 -0300

    feat: add prettier

---
+++ .prettierrc.cjs
@@ -0,0 +1,12 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  semi: true,
+  tabWidth: 2,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: 'none',
+  arrowParens: 'avoid',
+  endOfLine: 'auto',
+  bracketSpacing: true,
+  quoteProps: 'preserve'
+};
