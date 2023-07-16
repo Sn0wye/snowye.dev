c95f236b8e5f94e625010953fb1dce7fcc3a8f63
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2023-07-15 22:50:55 -0300

    fix: home metadata and typewriter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,27 @@
-import Head from 'next/head';
-// import Typewriter from 'typewriter-effect';
-import { Layout } from '../components/Layout';
-import { OpenCommandPalette } from '../components/OpenCommandPalette';
-import { home } from '../locales/en/pages/home';
-import { Container, Content, Title } from '../styles/home';
+import { type Metadata } from 'next';
+import { Layout } from '@/components/Layout';
+import { OpenCommandPalette } from '@/components/OpenCommandPalette';
+import { Container, Content, Title } from '@/styles/home';
+import { home } from '@/locales/en/pages/home';
+import { Typewriter } from './typewriter';
+
+export const metadata = {
+  title: home.title,
+  description: home.description,
+  openGraph: {
+    description: home.description
+  }
+} satisfies Metadata;
 
 export default function Home() {
   return (
     <Layout>
-      <Head>
-        <title>{home.title}</title>
-        <meta content={home.title} property="og:title" />
-        <meta content={home.description} name="description" />
-        <meta content={home.description} property="og:description" />
-      </Head>
       <Container>
         <Content>
           <Title>{home.title}</Title>
           <div>
             <strong>{home.meta}</strong>
-            {/* <Typewriter
-                options={{
-                  strings: home.description,
-                  autoStart: true,
-                  deleteSpeed: 80,
-                  delay: 60
-                }}
-              /> */}
+            <Typewriter strings={home.description} />
           </div>
           <OpenCommandPalette />
         </Content>
---
+++ src/app/typewriter.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import TypewriterEffect from 'typewriter-effect';
+
+type TypewriterProps = {
+  strings: string;
+};
+
+export const Typewriter = ({ strings }: TypewriterProps) => {
+  return (
+    <TypewriterEffect
+      options={{
+        strings,
+        autoStart: true,
+        deleteSpeed: 80,
+        delay: 60
+      }}
+    />
+  );
+};
