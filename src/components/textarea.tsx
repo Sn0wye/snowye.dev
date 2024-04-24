b07787a2a36063f5b0781cdd6de6b86ba805440e
Gabriel Trzimajewski <95551770+Sn0wye@users.noreply.github.com>
2024-04-23 22:01:29 -0300

    style: black bg on form

--- src/components/textarea.tsx
+++ src/components/textarea.tsx
@@ -9,7 +9,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
     return (
       <textarea
         className={cn(
-          'flex min-h-[80px] w-full rounded-md border border-zinc-800 bg-transparent px-3 py-2 text-sm shadow-sm placeholder:text-zinc-400 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-zinc-300 disabled:cursor-not-allowed disabled:opacity-50',
+          'flex min-h-[80px] w-full rounded-md border border-zinc-800 bg-black px-3 py-2 text-sm shadow-sm placeholder:text-zinc-400 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-zinc-300 disabled:cursor-not-allowed disabled:opacity-50',
           className
         )}
         ref={ref}
