f9281b917fd505ca99040a1c71028d284e01eb4a
Gabriel Trzimajewski <95551770+Sn0wye@users.noreply.github.com>
2024-05-22 13:10:12 -0300

    refac: remove unused layout component

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,7 +3,6 @@
 import Lottie from 'lottie-react';
 import dynamic from 'next/dynamic';
 import lottieAnimation from '../../public/static/icons/404.json';
-import { Layout } from '../components/layout';
 
 const ShortcutError = dynamic(() => import('@/components/shortcut-error'), {
   ssr: false
@@ -11,20 +10,18 @@ const ShortcutError = dynamic(() => import('@/components/shortcut-error'), {
 
 export default function NotFound() {
   return (
-    <Layout>
-      <div className="flex flex-1 flex-col items-center justify-center px-5 py-navHeightMobile">
-        <h1>four oh four</h1>
-        <div className="flex justify-center">
-          <Lottie
-            animationData={lottieAnimation}
-            loop={true}
-            autoPlay={true}
-            style={{ width: '60%' }}
-          />
-        </div>
-        <p>This page doesn&apos;t exist</p>
-        <ShortcutError />
+    <div className="flex flex-1 flex-col items-center justify-center px-5 py-navHeightMobile">
+      <h1>four oh four</h1>
+      <div className="flex justify-center">
+        <Lottie
+          animationData={lottieAnimation}
+          loop={true}
+          autoPlay={true}
+          style={{ width: '60%' }}
+        />
       </div>
-    </Layout>
+      <p>This page doesn&apos;t exist</p>
+      <ShortcutError />
+    </div>
   );
 }
