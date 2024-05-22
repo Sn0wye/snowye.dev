94fb99cfea0885039e54b698cbdf9e76aa4fd462
Gabriel Trzimajewski <95551770+Sn0wye@users.noreply.github.com>
2024-05-22 12:10:27 -0300

    fix: env

--- src/env.ts
+++ src/env.ts
@@ -4,7 +4,7 @@ import { vercel } from '@t3-oss/env-nextjs/presets';
 import { z } from 'zod';
 
 export const env = createEnv({
-  extends: [vercel],
+  extends: [vercel()],
   server: {
     NODE_ENV: z.enum(['development', 'test', 'production']),
     NODEMAILER_USER: z.string().min(1),
