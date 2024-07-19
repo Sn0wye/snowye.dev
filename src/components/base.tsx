309149938bc4383eada64cb2bbf54fb5f7a8fb16
Gabriel Trzimajewski <95551770+Sn0wye@users.noreply.github.com>
2024-07-19 10:41:10 -0300

    chore: remove unused imports

--- src/components/base.tsx
+++ src/components/base.tsx
@@ -1,8 +1,5 @@
 'use client';
 
-import { Footer } from './footer';
-import { Navbar } from './navbar';
-
 type Color = 'purple' | 'cyan' | 'yellow' | 'pink' | 'red' | 'orange' | 'green';
 
 interface BaseProps {
@@ -21,11 +18,11 @@ export function Base({
   title
 }: BaseProps) {
   return (
-    <div className="relative z-0 flex min-h-screen flex-col">
+    <div className='relative z-0 flex min-h-screen flex-col'>
       <main
         className={`selection: flex-1 overflow-hidden py-navHeightMobile md:py-navHeightDesktop selection:bg-geist-${primaryColor} selection:text-black`}
       >
-        <div className="relative z-10 mx-auto h-full max-w-3xl bg-transparent px-5 py-5 text-base leading-8 text-secondary">
+        <div className='relative z-10 mx-auto h-full max-w-3xl bg-transparent px-5 py-5 text-base leading-8 text-secondary'>
           <h1
             className={`bg-gradient-to-br decoration-clone bg-clip-text text-transparent from-geist-${primaryColor} to-geist-${secondaryColor}`}
           >
