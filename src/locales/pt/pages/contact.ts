493ceb73a58e24a9bfc3a7ace352b3d1f9cf982a
Gabriel Trzimajewski <95551770+Sn0wye@users.noreply.github.com>
2026-04-28 11:19:44 -0300

    style: change some typos

--- src/locales/pt/pages/contact.ts
+++ src/locales/pt/pages/contact.ts
@@ -2,7 +2,7 @@ export const contact = {
   title: 'Contato | Gabriel Trzimajewski',
   tagline: 'Manda um email. Como nos velhos tempos.',
   description:
-    '<strong>Amo conversar</strong> com engenheiros de software, desenvolvedores no geral, estudantes, and criadores. <strong>Sou uma pessoa ocupada</strong>, então não posso prometer que vou responder ao seu e-mail na hora, mas vou dar o meu melhor para responder com tempo.',
+    '<strong>Amo conversar</strong> com engenheiros de software, desenvolvedores no geral, estudantes, e criadores. <strong>Sou uma pessoa ocupada</strong>, então não posso prometer que vou responder ao seu e-mail na hora, mas vou dar o meu melhor para responder com tempo.',
   email: 'Mande um email',
   toast: {
     success: {
